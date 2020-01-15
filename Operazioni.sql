498b6372ab9aa80a1d74a0ae109adc7e198aabf8
Joe-cyber <51990341+Joe-cyber@users.noreply.github.com>
2020-01-15 17:21:19 +0100

    Update Operazioni.sql

--- Operazioni.sql
+++ Operazioni.sql
@@ -50,26 +50,22 @@ insert into telefono_fax(Numero,Tipo,Cliente_CodiceC) values(?,?,?);
 
 insert into sistema_operativo(Sistema,Software_CodiceS) values (?,?);
 
-
-/*12. Aggiungere un Attestato conseguito da un Cliente.*/
-insert into attestato(Data_rilascio,Corso_Formativo_CodiceCF,Cliente_CodiceC) values ( ?,?,?);
-
 /*VISUALIZZARE*/
 
+/*12.Stampare il numero di esami conseguiti da un Privato per un Corso Formativo.*/
 
-/*13.Stampare il numero di esami conseguiti da un Privato per un Corso Formativo.*/
 select corso_formativo.CodiceCF,corso_formativo.Descrizione,corso_formativo.Numero_esami,segue.numero_esami_dati 
 from privato inner join segue on privato.Cliente_CodiceC=segue.Cliente_CodiceC
 inner join corso_formativo on segue.Corso_Formativo_CodiceCF=corso_formativo.CodiceCF
 where privato.Cliente_CodiceC= 4;
 
-/*14.Visualizzare il numero di esami mancanti per ogni Cliente inerenti ad un Corso Formativo.*/
+/*13.Visualizzare il numero di esami mancanti per ogni Cliente inerenti ad un Corso Formativo.*/
 
 select privato.CF, privato.Nome, privato.Cognome, privato.Data_nascita, azienda.Partita_iva, azienda.Ragione_sociale, cliente.Indirizzo, corso_formativo.numero_esami-segue.Numero_esami_dati as Esami_mancanti
 from segue inner join cliente on segue.Cliente_CodiceC=cliente.CodiceC left join privato on cliente.CodiceC=privato.Cliente_CodiceC left join azienda on cliente.CodiceC=azienda.Cliente_CodiceC inner join corso_formativo on corso_formativo.CodiceCF=segue.Corso_Formativo_CodiceCF
 where corso_formativo.CodiceCF=2;
 
-/*15.Mostrare quali Azienda hanno conseguito almeno n esami.*/
+/*14.Mostrare quali Azienda hanno conseguito almeno n esami.*/
 
 select distinct (cliente.CodiceC), azienda.Partita_iva, azienda.Ragione_sociale, cliente.Indirizzo, telefono_fax.Numero, telefono_fax.Tipo 
 from azienda inner join cliente on azienda.Cliente_CodiceC=cliente.CodiceC
@@ -77,19 +73,19 @@ inner join telefono_fax on cliente.CodiceC=telefono_fax.Cliente_CodiceC
 inner join segue on segue.Cliente_codiceC=cliente.CodiceC
 where segue.numero_esami_dati>2 and segue.Corso_Formativo_CodiceCF=4;
 
-/*16.Visualizzare un Operatore che ha preso in carico un determinato Problema. */
+/*15.Visualizzare un Operatore che ha preso in carico un determinato Problema. */
 
 select operatore.*
 from operatore inner join problema on operatore.CodiceO=problema.Operatore_CodiceO
 where problema.Software_CodiceS=6 AND problema.Numero=1;
 
-/*17.Stampare tutti i Sistemi Operativi compatibili per uno specifico Software.*/
+/*16.Stampare tutti i Sistemi Operativi compatibili per uno specifico Software.*/
 
 select sistema_operativo.Sistema
 from  sistema_operativo
 where sistema_operativo.Software_CodiceS=1;
 
-/*18.Mostrare tutti i Clienti che hanno speso un totale di p Euro.*/
+/*17.Mostrare tutti i Clienti che hanno speso un totale di p Euro.*/
 
 select cliente.*, privato.CF, privato.Nome, privato.Cognome, azienda.Ragione_sociale, azienda.Partita_iva
 from cliente left join privato on privato.Cliente_CodiceC=cliente.CodiceC left join azienda on azienda.Cliente_CodiceC=cliente.CodiceC inner join acquista on cliente.CodiceC=acquista.Cliente_CodiceC
@@ -97,53 +93,50 @@ inner join software on software.CodiceS=acquista.Software_CodiceS
 group by cliente.CodiceC
 having sum(prezzo)>100;
 
-
-/*19.Visualizzare l'elenco di tutti gli Attestati conseguiti da un Cliente.*/
+/*18.Visualizzare l'elenco di tutti gli Attestati conseguiti da un Cliente.*/
 
 select privato.CF, privato.Nome, privato.Cognome, azienda.Ragione_sociale, azienda.Partita_iva, attestato.Corso_Formativo_CodiceCF,corso_formativo.Descrizione,attestato.data_rilascio, cliente.Indirizzo
 from cliente inner join attestato on cliente.CodiceC=attestato.Cliente_CodiceC left join privato on privato.Cliente_CodiceC=cliente.CodiceC left join azienda on azienda.Cliente_CodiceC=cliente.CodiceC inner join corso_formativo on attestato.Corso_Formativo_CodiceCF=corso_formativo.CodiceCF
 where attestato.Cliente_CodiceC=3;
 
-
-/*20.Stampare tutte le Aziende alle quali potrebbero interessare dei Software in base agli acquisti, inserito un tipo di riferimento. */
+/*19.Stampare tutte le Aziende alle quali potrebbero interessare dei Software in base agli acquisti, inserito un tipo di riferimento. */
 select azienda.*
+
 from software inner join acquista on software.CodiceS=acquista.Software_CodiceS
 inner join cliente on cliente.CodiceC=acquista.Cliente_CodiceC
 inner join azienda on azienda.Cliente_CodiceC=cliente.CodiceC
 where software.tipo='Firmware';
 
-/*21.Visualizzare i clienti che hanno conseguito un numero di esami pari a quelli previsti dal corso formativo*/
+/*20.Visualizzare i clienti che hanno conseguito un numero di esami pari a quelli previsti dal corso formativo*/
  
 select  privato.CF, privato.Nome, privato.Cognome, azienda.Ragione_sociale, azienda.Partita_iva, cliente.CodiceC,cliente.Indirizzo, corso_formativo.CodiceCF,corso_formativo.Descrizione
 from cliente left join privato on privato.Cliente_CodiceC=cliente.CodiceC left join azienda on azienda.Cliente_CodiceC=cliente.CodiceC inner join segue on cliente.codiceC=segue.Cliente_CodiceC
 join corso_formativo  on segue.Corso_Formativo_CodiceCF=corso_formativo.CodiceCF
 where segue.Numero_esami_dati=corso_formativo.Numero_esami;
 
-/*22.Visualizzare quale Software ha avuto piu Problemi*/
+/*21.Visualizzare quale Software ha avuto piu Problemi*/
 
 select software.*
 from software
 where software.CodiceS=(select software_CodiceS
-						from problema
-						where numero=(select max(numero)
-										from problema));
+			from problema
+			where numero=(select max(numero)
+				      from problema));
 
-/*23.Visualizzare l'operatore che si prende carico di un Problema di un determinato Software.*/
+/*22.Visualizzare l'operatore che si prende carico di un Problema di un determinato Software.*/
 
 select operatore.*
 from operatore inner join problema on operatore.CodiceO=problema.Operatore_CodiceO
 where problema.software_CodiceS=6 AND problema.numero=1;
 
-
-/*24.Quanti Clienti hanno acquistato un Software.*//*da fare*/
-
+/*23.Quanti Clienti hanno acquistato un Software.*//*da fare*/
 
 
-/*Visualizzare i numeri di Telefono/Fax di un Cliente*/
-select telefono_fax.numero
+
+/*24.Visualizzare i numeri di Telefono/Fax di un Cliente*/
+select telefono_fax.numero, telefono_fax.tipo
 from telefono_fax
 where telefono_fax.Cliente_CodiceC=1;
-
 
 /*AGGIARNAMENTO*/
 
@@ -151,9 +144,8 @@ where telefono_fax.Cliente_CodiceC=1;
 
 update segue
 set segue.numero_esami_dati=(select numero_esami_dati
-							where segue.Cliente_CodiceS=? and  segue.Corso_formativo_CodiceCF=?)+1
+			     where segue.Cliente_CodiceS=? and  segue.Corso_formativo_CodiceCF=?)+1
 where segue.Cliente_CodiceC=? and segue.Corso_formativo_CodiceCF=?;
-
 
 /*26.Aggiornare il prezzo di un dato Software.*/
 
